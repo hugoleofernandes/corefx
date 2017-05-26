60c4b38d2822d8a324cdeb13cc84bc3100ed1b43
Atsushi Kanamori <atsushik@microsoft.com>
2017-05-26 13:59:25 -0700

    sre-wip 5/26/2017 1:59:24 PM

--- src/System.Runtime.Extensions/tests/System/Math.cs
+++ src/System.Runtime.Extensions/tests/System/Math.cs
@@ -657,17 +657,6 @@ namespace System.Tests
         }
 
         [Theory]
-        [InlineData(-1.0,                      double.PositiveInfinity, -0.0,                 0.0)]
-        [InlineData(-0.0,                      double.NegativeInfinity, -3.1415926535897932,  CrossPlatformMachineEpsilon * 10)]    // expected: -(pi)
-        [InlineData(-0.0,                     -1.0,                     -3.1415926535897932,  CrossPlatformMachineEpsilon * 10)]    // expected: -(pi)
-        [InlineData(-0.0,                     -0.0,                     -3.1415926535897932,  CrossPlatformMachineEpsilon * 10)]    // expected: -(pi)
-        [InlineData( 0.0,                     -0.0,                      3.1415926535897932,  CrossPlatformMachineEpsilon * 10)]     // expected:  (pi)
-        public static void Atan2_UapAotBug(double y, double x, double expectedResult, double allowedVariance)
-        {
-            AssertEqual(expectedResult, Math.Atan2(y, x), allowedVariance);
-        }
-
-        [Theory]
         [InlineData( double.NegativeInfinity, double.NegativeInfinity, -2.3561944901923449,  CrossPlatformMachineEpsilon * 10)]    // expected: -(3 * pi / 4)
         [InlineData( double.NegativeInfinity, double.PositiveInfinity, -0.78539816339744831, CrossPlatformMachineEpsilon)]         // expected: -(pi / 4)
         [InlineData( double.PositiveInfinity, double.NegativeInfinity,  2.3561944901923449,  CrossPlatformMachineEpsilon * 10)]    // expected:  (3 * pi / 4)
@@ -1175,7 +1164,6 @@ namespace System.Tests
 
         [Theory]
         [InlineData( double.NegativeInfinity,  double.NegativeInfinity,  0.0,                     0.0)]
-        //CHECKINBUGBUG [InlineData( double.NegativeInfinity, -1.0,                     -0.0,                     0.0)]
         [InlineData( double.NegativeInfinity, -0.0,                      1.0,                     CrossPlatformMachineEpsilon * 10)]
         [InlineData( double.NegativeInfinity,  double.NaN,               double.NaN,              0.0)]
         [InlineData( double.NegativeInfinity,  0.0,                      1.0,                     CrossPlatformMachineEpsilon * 10)]
@@ -1209,10 +1197,8 @@ namespace System.Tests
         [InlineData(-1.0,                      0.0,                      1.0,                     CrossPlatformMachineEpsilon * 10)]
         [InlineData(-1.0,                      1.0,                     -1.0,                     CrossPlatformMachineEpsilon * 10)]
         [InlineData(-0.0,                      double.NegativeInfinity,  double.PositiveInfinity, 0.0)]
-        //CHECKINBUGBUG [InlineData(-0.0,                     -3.0,                      double.NegativeInfinity, 0.0)]
         [InlineData(-0.0,                     -2.0,                      double.PositiveInfinity, 0.0)]
         [InlineData(-0.0,                     -1.5707963267948966,       double.PositiveInfinity, 0.0)]                                     //          y: -(pi / 2)
-        //CHECKINBUGBUG [InlineData(-0.0,                     -1.0,                      double.NegativeInfinity, 0.0)]
         [InlineData(-0.0,                     -0.0,                      1.0,                     CrossPlatformMachineEpsilon * 10)]
         [InlineData(-0.0,                      double.NaN,               double.NaN,              0.0)]
         [InlineData(-0.0,                      0.0,                      1.0,                     CrossPlatformMachineEpsilon * 10)]
@@ -1321,6 +1307,26 @@ namespace System.Tests
         public static void Pow(double x, double y, double expectedResult, double allowedVariance)
         {
             AssertEqual(expectedResult, Math.Pow(x, y), allowedVariance);
+        }
+
+        [Theory]
+        [InlineData( double.NegativeInfinity, -1.0,                     -0.0,                     0.0)]
+        [InlineData(-0.0,                     -3.0,                      double.NegativeInfinity, 0.0)]
+        [InlineData(-0.0,                     -1.0,                      double.NegativeInfinity, 0.0)]
+        public static void Pow_AotMetadataBug(double x, double y, double expectedResult, double allowedVariance)
+        {
+            AssertEqual(expectedResult, Math.Pow(x, y), allowedVariance);
+        }
+
+        [Theory]
+        [InlineData(-1.0, double.PositiveInfinity, -0.0, 0.0)]
+        [InlineData(-0.0, double.NegativeInfinity, -3.1415926535897932, CrossPlatformMachineEpsilon * 10)]    // expected: -(pi)
+        [InlineData(-0.0, -1.0, -3.1415926535897932, CrossPlatformMachineEpsilon * 10)]    // expected: -(pi)
+        [InlineData(-0.0, -0.0, -3.1415926535897932, CrossPlatformMachineEpsilon * 10)]    // expected: -(pi)
+        [InlineData(0.0, -0.0, 3.1415926535897932, CrossPlatformMachineEpsilon * 10)]     // expected:  (pi)
+        public static void Atan2_AotMetadataBug(double y, double x, double expectedResult, double allowedVariance)
+        {
+            AssertEqual(expectedResult, Math.Atan2(y, x), allowedVariance);
         }
 
         [Theory]
